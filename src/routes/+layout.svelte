5f648a1b7616a45205d49f6abf793fd4afd6312b
Youwen Wu <youwenw@gmail.com>
2024-03-11 13:21:44 -0700

    chore: add base styles and deps

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import '$lib/app.css'
+  import '@fontsource/inter/latin.css'
+  import '@fontsource/fira-code/latin.css'
+  import '@fontsource/ibm-plex-serif/latin.css'
+</script>
+
+<slot />
