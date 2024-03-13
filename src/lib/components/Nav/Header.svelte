bd09a54eb417a91ef5ab81025c1b67a56460e6d6
Youwen Wu <youwenw@gmail.com>
2024-03-12 18:42:12 -0700

    feat: add prisma for database interfacing

--- src/lib/components/Nav/Header.svelte
+++ src/lib/components/Nav/Header.svelte
@@ -1,3 +1,23 @@
+<script lang="ts">
+  import { page } from '$app/stores'
+
+  $: segments = $page.url.pathname.split('/')
+
+  let current: 'cards' | 'create' | 'about' | 'home'
+
+  $: {
+    if (segments[1] === 'cards' && segments[2] === 'create') {
+      current = 'create'
+    } else if (segments[1] === 'cards') {
+      current = 'cards'
+    } else if (segments[1] === 'about') {
+      current = 'about'
+    } else if (segments[1] === '') {
+      current = 'home'
+    }
+  }
+</script>
+
 <header>
   <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 w-full fixed z-50">
     <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
@@ -54,21 +74,20 @@
           >
         </button>
       </div>
-      <div
-        class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-        id="mobile-menu-2"
-      >
+      <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1">
         <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
           <li>
             <a
               href="/"
-              class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
+              class:active={current === 'home'}
+              class="block py-2 pr-4 pl-3 text-gray-700 dark:text-gray-400 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0"
               aria-current="page">Home</a
             >
           </li>
           <li>
             <a
               href="/cards"
+              class:active={current === 'cards'}
               class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
               >Cards</a
             >
@@ -76,6 +95,7 @@
           <li>
             <a
               href="/cards/create"
+              class:active={current === 'create'}
               class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
               >Create</a
             >
@@ -83,6 +103,7 @@
           <li>
             <a
               href="/about"
+              class:active={current === 'about'}
               class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
               >About</a
             >
@@ -92,3 +113,9 @@
     </div>
   </nav>
 </header>
+
+<style lang="postcss">
+  .active {
+    @apply text-white dark:text-white;
+  }
+</style>
