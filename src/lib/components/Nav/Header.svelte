b3019c9970fa100e3963f65449c53fdffaa425aa
Youwen Wu <youwenw@gmail.com>
2024-03-12 17:36:51 -0700

    feat: add links to header

--- src/lib/components/Nav/Header.svelte
+++ src/lib/components/Nav/Header.svelte
@@ -1,5 +1,5 @@
 <header>
-  <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 w-full fixed">
+  <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 w-full fixed z-50">
     <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
       <a href="/" class="flex items-center">
         <img
@@ -13,12 +13,12 @@
       </a>
       <div class="flex items-center lg:order-2">
         <a
-          href="#"
+          href="/account/login"
           class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
           >Log in</a
         >
         <a
-          href="#"
+          href="/account/create"
           class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
           >Get started</a
         >
@@ -61,7 +61,7 @@
         <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
           <li>
             <a
-              href="#"
+              href="/"
               class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
               aria-current="page">Home</a
             >
